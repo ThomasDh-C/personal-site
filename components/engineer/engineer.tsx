d2c6c4a70de1a25fc4cfe25430c10efb8b046f02
Thomas Dhome-Casanova <tmd4@princeton.edu>
2020-12-19 16:55:31 +0000

    all icons done, labels made into a component

--- components/engineer/engineer.tsx
+++ components/engineer/engineer.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import Princeton from '../../public/PU.svg'
+import HighlightLabel from './highlightLabel'
+import Rocket from '../../public/rocketbright.svg'
+import Chip from '../../public/chip.svg'
+import Surelight from '../../public/surelighticon.svg'
 
 const BackgroundText = styled.h1`
   font-size: 15vw;
@@ -11,15 +14,58 @@ const BackgroundText = styled.h1`
   opacity: 0.5;
 `
 
-const Logo = styled.img`
-    fill: white;
+const RocketStyled = styled(Rocket)`
+    position: absolute;
+    height: 70vh;
+    left: 28vw;
+    top: 30vh;
+
+    opacity: 70%;
+    
+    &:hover {
+        transition: 300ms;
+        opacity: 100%;
+      }
 `
+const ChipStyled = styled(Chip)`
+    position: absolute;
+    height: 25vh;
+    left: 40vw;
+    top: 33vh;
+
+    opacity: 70%;
+    
+    &:hover {
+        transition: 300ms;
+        opacity: 100%;
+    }
+`
+
+
+const SurelightStyled = styled(Surelight)`
+    position: absolute;
+    height: 40vh;
+    left: 45vw;
+    top: 60vh;
+
+    opacity: 70%;
+
+    &:hover {
+        transition: 300ms;
+        opacity: 100%;
+    }
+`
+
+
+
 const Engineer = () => {
     return (
         <>
             <BackgroundText>Engineer</BackgroundText>
-            {/* <Logo src={princeton}/> */}
-            <Princeton fill="white" />
+            <HighlightLabel label="Gimbal Motor Design"/>
+            <RocketStyled />
+            <ChipStyled />
+            <SurelightStyled />
         </>
     )
 }
